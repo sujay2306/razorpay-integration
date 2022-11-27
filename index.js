57a9c30f602c816d273db64bef201a654dd17007
sujay2306 <sujayks007@gmail.com>
2022-11-27 14:55:07 +0530

    added razor pay integration to basic front-end

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const Razorpay = require("razorpay")
+app.use(express.static("./public"))
+// app.get("/", (req, res) => {
+//     res.send("hello");
+//     console.log("hello this is backend");
+// });
 
-app.get("/", (req, res) => {
-    res.send("hello");
-    console.log("hello this is backend");
-});
+app.post("/order", async (req, res) => {
 
-app.post("/order", (req, res) => {
-    
     var amount = req.body.amount;
-    var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' });
 
+    //this needs to go in .env file
+    var instance = new Razorpay({ 
+       key_id: 'rzp_test_PwptW1080n38Ts', 
+        key_secret: 'Zs5QbTmxHdduGzwhPRZz9ouA'
+    });
+       
     var options = {
         amount: 50000,
         currency: "INR",
         receipt: "receipt#1",
     };
 
-    var myOrders = instance.orders.create(options)
+    var myOrder = await instance.orders.create(options)
     res.status(200).json({
         success: "true",
         amount,
